f611f69e296406b65058aece9c48ec53fdb83217
Priya Khadke <priyakhadke15@gmail.com>
2019-04-21 17:47:27 -0700

    Updated POST call for add farm

--- UI/public/javascripts/script.js
+++ UI/public/javascripts/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     const userAndScope = getUserAndScope();
     if (userAndScope) {
-        const { username, scope } = userAndScope;
+        const {
+            username,
+            scope
+        } = userAndScope;
         $("#btnLogin").text(username)
         $("#btnDashboard").css("display", "");
         if (scope === "admin") {
@@ -13,6 +16,7 @@ $(document).ready(function() {
 
     $("#addSensorButton").click(event => {
         event.preventDefault();
+
         const dataToPost = {
             inp1: $("#inputSensorName").val(),
             inp2: $("#inputSensorManufacturer").val(),
@@ -32,27 +36,42 @@ $(document).ready(function() {
         });
     })
 
-       $("#addFarmButton").click(event => {
+    $("#addFarmButton").click(event => {
         event.preventDefault();
+
         const dataToPost = {
-        	farmerId: $("#inputFarmerID").val(),
-            farmId: $("#inputFarmID").val(),
-            ranchId: $("#inputRanchID").val(),
-            clusterId: $("#inputClusterID").val(),
-            sensorId: $("#inputSensorID").val(),
-            temp: $("#btemperature").is(":checked"),
-            humidity: $("#bhumidity").is(":checked"),
-            soilMoisture: $("#bsoilMoisture").is(":checked"),
-            windSpeed: $("#bwindSpeed").is(":checked"),
-            rain: $("#brain").is(":checked"),
+            farm: {
+                farmerId: $("#inputFarmerID").val(),
+                farmId: $("#inputFarmID").val(),
+                ranches: [{
+                    ranchId: $("#inputRanchID").val(),
+                    clusters: [{
+                        clusterId: $("#inputClusterID").val(),
+                        sensors: []
+                    }]
+                }]
+            }
         };
-        alert(JSON.stringify(dataToPost, null, 4));
-        // $.ajax({
-        //     method: "POST",
-        //     url: "http://localhost:1880/sensors",
-        //     data: dataToPost
-        // }).done(function(response) {
-        //     alert(JSON.stringify(response, null, 4));
-        // });
+        const sensorTemplate = {
+            sensorId: $("#inputSensorID").val(),
+            status: "ON"
+        };
+        const checkboxIds = ["temperature", "humidity", "soilMoisture", "windSpeed", "rain"];
+        checkboxIds.forEach(type => {
+        	if($(`#b${type}`).is(":checked")) {
+        		dataToPost.farm.ranches[0].clusters[0].sensors.push({
+        			...sensorTemplate,
+        			type
+        		});
+        	}
+        });
+        console.log(dataToPost);
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:1880/farms",
+            data: dataToPost
+        }).done(function(response) {
+            alert(JSON.stringify(response, null, 4));
+        });
     })
 });
