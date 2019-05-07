fa760379f57228b67672394b33a43be7e6e2ee3c
Ubuntu <ubuntu@ip-172-31-6-205.us-west-1.compute.internal>
2019-05-07 17:20:35 +0000

    updated dashboard-server flows

--- dashboard-server/customizations/run.sh
+++ dashboard-server/customizations/run.sh
@@ -1,5 +1,6 @@
 git submodule update --init --recursive
 cd ../node-red/
+git reset --hard a2d03c14ae26a0e53beefa2ba4d07232778f9814
 cp ../customizations/settings.js .
 cp ../customizations/flows.json .
 npm i -S node-red-contrib-cassandra node-red-contrib-users node-red-contrib-web-worldmap node-red-dashboard node-red-node-data-generator
