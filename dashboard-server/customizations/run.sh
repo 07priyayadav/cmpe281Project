b567fc8e6c20ae53c96ac492e551b380c095a8c8
Priya Khadke <priyakhadke15@gmail.com>
2019-05-07 08:50:58 -0700

    added dashboard-server as a separate module (git submodule)

---
+++ dashboard-server/customizations/run.sh
@@ -0,0 +1,8 @@
+git submodule update --init --recursive
+cd ../node-red/
+cp ../customizations/settings.js .
+cp ../customizations/flows.json .
+npm i -S node-red-contrib-cassandra node-red-contrib-users node-red-contrib-web-worldmap node-red-dashboard node-red-node-data-generator
+npm install
+npm run build
+rm .flows.json.backup
